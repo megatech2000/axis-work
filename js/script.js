012c51a787da728f89b01f4159faee338b4b0b82
megatech2000 <151417898+megatech2000@users.noreply.github.com>
2024-04-01 16:19:28 +0530

    updating
    grid updating

--- js/script.js
+++ js/script.js
@@ -26,11 +26,15 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-window.addEventListener("scroll", function () {
-  let navbar = document.getElementById("navbar");
-  if (window.scrollY > 0) {
-    navbar.classList.add("navbar-scroll");
-  } else {
-    navbar.classList.remove("navbar-scroll");
-  }
+// window.addEventListener("scroll", function () {
+//   const navbar = document.getElementById("navbar");
+//   if (window.scrollY > 0) {
+//     navbar.classList.add("navbar-scroll");
+//   } else {
+//     navbar.classList.remove("navbar-scroll");
+//   }
+// });
+
+window.addEventListener("scroll", () => {
+  console.log("scrolled!!!");
 });
